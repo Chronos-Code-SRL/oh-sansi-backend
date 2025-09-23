9f6e974a669ad7e499cc5f29dd7b25f2f269fced
Carlos Vargas <esteban.vg00@gmail.com>
2025-09-23 01:51:19 -0400

    feat: methods in Area and Phase models to fully connect to Olympiad model implemented

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -11,8 +11,25 @@ class Area extends Model
 
     protected $fillable = ['name', 'description'];
 
+    public function olympiads()
+    {
+        return $this->belongsToMany(Olympiad::class, 'olympiad_areas');
+    }
+
     public function olympiadAreas()
     {
         return $this->hasMany(OlympiadArea::class);
+    }
+
+    public function phases()
+    {
+        return $this->hasManyThrough(
+            OlympiadAreaPhase::class,   // Destination model
+            OlympiadArea::class,        // Intermediate model
+            'area_id',                  // FK in olympiad_areas
+            'olympiad_area_id',         // FK in olympiad_area_phases
+            'id',                       // PK in areas
+            'id'                        // PK in olympiad_areas
+        );
     }
 }
--- app/Models/Phase.php
+++ app/Models/Phase.php
@@ -11,6 +11,11 @@ class Phase extends Model
 
     protected $fillable = ['name', 'order'];
 
+    public function olympiadAreas()
+    {
+        return $this->belongsToMany(OlympiadArea::class, 'olympiad_area_phases');
+    }
+
     public function olympiadAreaPhases()
     {
         return $this->hasMany(OlympiadAreaPhase::class);
