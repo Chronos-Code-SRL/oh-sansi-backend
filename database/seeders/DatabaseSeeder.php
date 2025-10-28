1db36aca9ae35c4c8d34a07bb8b9c8a099c0ad9a
Andres Chavez <t4t4x777@gmail.com>
2025-10-28 00:18:44 -0400

    feat: add the seed for admin grades and areas in a file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Grade;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $grades = [
+            // Primaria
+            ['name' => 'Primero de primaria'],
+            ['name' => 'Segundo de primaria'],
+            ['name' => 'Tercero de primaria'],
+            ['name' => 'Cuarto de primaria'],
+            ['name' => 'Quinto de primaria'],
+            ['name' => 'Sexto de primaria'],
+
+            // Secundaria
+            ['name' => 'Primero de secundaria'],
+            ['name' => 'Segundo de secundaria'],
+            ['name' => 'Tercero de secundaria'],
+            ['name' => 'Cuarto de secundaria'],
+            ['name' => 'Quinto de secundaria'],
+            ['name' => 'Sexto de secundaria']
+        ];
+
+        foreach ($grades as $grade) {
+            Grade::firstOrCreate($grade);
+        }
+
+        $areas = [
+            ['name' => 'Astronomía'],
+            ['name' => 'Biología'],
+            ['name' => 'Física'],
+            ['name' => 'Informática'],
+            ['name' => 'Matemática'],
+            ['name' => 'Química'],
+            ['name' => 'Robótica'],
+            ['name' => 'Astrofísica']
+        ];
+
+        foreach ($areas as $area) {
+            \App\Models\Area::firstOrCreate($area);
+        }
+
         $this->call([
             AreaSeeder::class,
             LevelSeeder::class,
@@ -21,14 +60,17 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $user = new User();
-        $user->first_name = 'Admin';
-        $user->last_name = 'User';
-        $user->email = 'admin@example.com';
+        $user->first_name = 'Root';
+        $user->last_name = 'Admin';
+        $user->email = 'admin@gmail.com';
         $user->password = bcrypt('password');
         $user->ci = '12345678';
-        $user->phone_number = '+59 12345678';
+        $user->phone_number = '+59 71780589';
         $user->genre = 'femenino';
         $user->roles_id = 1;
         $user->save();
+
+        $this->command->info('Grades, Areas and user admin created successfully.');
+
     }
 }
