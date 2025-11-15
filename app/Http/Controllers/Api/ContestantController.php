f32f834188a6afe8b3159c2700075149cf0e24ac
Andres Chavez <t4t4x777@gmail.com>
2025-11-15 11:07:14 -0400

    feat: add the olympics id to the controller to get competitors

--- app/Http/Controllers/Api/ContestantController.php
+++ app/Http/Controllers/Api/ContestantController.php
@@ -77,19 +77,8 @@ class ContestantController extends Controller
         return response()->json($result);
     }
 
-    public function showContestantsOlympiad(): JsonResponse
+    public function showContestantsOlympiad($olympiad_id): JsonResponse
     {
-
-        $activeOlympiad = DB::table('olympiads')
-            ->where('status', '=', 'Activa')
-            ->first();
-
-        if (!$activeOlympiad) {
-            return response()->json([
-                'message' => 'There are no active Olympiads currently',
-                'status' => 404
-            ], 404);
-        }
 
         $contestantsOlympiad = DB::table('contestants')
             ->join('registrations', 'registrations.contestant_id', '=', 'contestants.id')
@@ -100,7 +89,7 @@ class ContestantController extends Controller
             ->leftJoin('level_grades', 'contestant_level_grades.level_grade_id', '=', 'level_grades.id')
             ->leftJoin('grades', 'level_grades.grade_id', '=', 'grades.id')
             ->leftJoin('levels', 'level_grades.level_id', '=', 'levels.id')
-            ->where('olympiad_areas.olympiad_id', $activeOlympiad->id)
+            ->where('olympiad_areas.olympiad_id', $olympiad_id)
             ->select(
                 'contestants.id AS contestant_id',
                 'evaluations.id AS evaluation_id',
