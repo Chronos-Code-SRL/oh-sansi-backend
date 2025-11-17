62bac854eb3a0f8eb4b83fab6b096ef3c82ec732
Andres Chavez <t4t4x777@gmail.com>
2025-11-16 23:49:52 -0400

    feat: add the function changeUserRole

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -87,20 +87,25 @@ class AuthController extends Controller
         // search user by ci
         $user = User::where('ci', $request->ci)->first();
         if ($user) {
-            $userRole = DB::table('user_roles')
+            
+            $currentRole = DB::table('user_roles')
                 ->where('user_id', $user->id)
-                ->where('role_id', $request->roles_id)
                 ->first();
 
-            if (!$userRole) {
-                $userRoleId = DB::table('user_roles')->insertGetId([
-                    'user_id' => $user->id,
-                    'role_id' => $request->roles_id,
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
+            if ($currentRole && $currentRole->role_id != $request->roles_id) {
+                $userRoleId = $this->changeUserRole($user, $request->roles_id);
+
             } else {
-                $userRoleId = $userRole->id;
+                if (!$currentRole) {
+                    $userRoleId = DB::table('user_roles')->insertGetId([
+                        'user_id' => $user->id,
+                        'role_id' => $request->roles_id,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ]);
+                } else {
+                    $userRoleId = $currentRole->id;
+                }
             }
 
             $this->syncUserAreasOlympiad($userRoleId, $request->areas_id, $request->olympiad_id);
@@ -331,5 +336,33 @@ class AuthController extends Controller
                 'areas' => []
             ]);
         }
+    }
+
+    private function changeUserRole($user, $newRoleId)
+    {
+        // search old role
+        $oldRole = DB::table('user_roles')
+            ->where('user_id', $user->id)
+            ->first();
+
+        if ($oldRole) {
+            // delete areas associated with old role
+            DB::table('user_area_olympiads')
+                ->where('user_role_id', $oldRole->id)
+                ->delete();
+
+            // delete rol
+            DB::table('user_roles')
+                ->where('id', $oldRole->id)
+                ->delete();
+        }
+
+        // create new role
+        return DB::table('user_roles')->insertGetId([
+            'user_id' => $user->id,
+            'role_id' => $newRoleId,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
